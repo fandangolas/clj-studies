3eedca8e81d00a53ce590b382d16ef0aeb86f2c6
Nícolas <nicolas.knoxx@gmail.com>
2020-11-16 04:53:40 -0300

    adding * to diff from original fn

--- clj-recursion-101/src/length.clj
+++ clj-recursion-101/src/length.clj
@@ -2,27 +2,27 @@
 
 ;Defining length fn using recursion
 
-(defn length [coll]
+(defn length* [coll]
   (let [empty-result 0
         coll-minus-first (rest coll)]
     (if
       (empty? coll)
       empty-result
-      (+ 1 (length coll-minus-first)))))
+      (inc (length* coll-minus-first)))))
 
-(length [])
-(length [1])
-(length (range 10))
+(length* [])
+(length* [1])
+(length* (range 10))
 
 ;Defining length fn with tail-recursion
 
-(defn length-with-tail-recursion [coll]
+(defn length-with-tail-recursion* [coll]
   (loop [coll coll acc 0]
     (if (empty? coll)
       acc
       (let [rest (rest coll)]
         (recur rest (inc acc))))))
 
-(length-with-tail-recursion [])
-(length-with-tail-recursion [1])
-(length-with-tail-recursion (range 10))
+(length-with-tail-recursion* [])
+(length-with-tail-recursion* [1])
+(length-with-tail-recursion* (range 10))
